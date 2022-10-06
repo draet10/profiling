2deaec2d385826a1221fad77a9f6c6665c2bb6e8
shivamk2 <shivamk2@sinkhole.esl.cisco.com>
2022-10-06 22:45:05 +0530

    Initial

---
+++ testing-compilation/mmsmart.c
@@ -0,0 +1,55 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+#define MSIZE 2000
+
+double matrix_a[MSIZE][MSIZE];
+double matrix_b[MSIZE][MSIZE];
+double matrix_r[MSIZE][MSIZE];
+
+void initialize_matrix(){
+	//printf("Initializing Matrices\n");
+	int i,j;
+	for(i=0;i<MSIZE;i++){
+		for(j=0;j<MSIZE;j++){
+			matrix_a[i][j]= (double) rand() / RAND_MAX;
+			matrix_b[i][j]= (double) rand() / RAND_MAX;
+			matrix_r[i][j]= 0.0;
+		}	
+	}
+	//printf("Intialized Matrices\n");
+
+}
+
+void multiply_matrix(){
+	int i,j,k;
+	//printf("Matrix multiplying\n Wait for 3-4 mins\n");
+
+	for(i=0;i<MSIZE;i++){
+		for(k=0;k<MSIZE;k++){
+            //double sum =0.0 
+			for(j=0;j<MSIZE;j++)
+				matrix_r[i][j] += matrix_a[i][k]*matrix_b[k][j];	
+            //matrix_r[i][j] = sum;			
+		}
+	}
+	//printf("\nMatrix multiplied\n");
+}
+
+void print_result(){
+	int i,j;
+	for(i=0;i<MSIZE;i++){
+		for(j=0;j<MSIZE;j++){
+			//printf("%lf \t", matrix_r[i][j]);
+		}	
+		//printf("\n");
+	}
+}
+
+int main(){
+	//printf("Testing matrix multiplication unoptimized \n");
+	initialize_matrix();
+	//printf("Starting the multiplication of Matrix A  & B: -  \n");
+	multiply_matrix();
+	//print_result();
+}
---
+++ testing-compilation/test-compilation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("Hello World!!\n");
+    int a = 1;
+    int b = 4;
+    int c = a;
+    int d = b + c;
+    printf("Sum of %d and %d is equal to %d\n", b, c, d);
+}
