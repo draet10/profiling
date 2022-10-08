47ee4d7aebed604901892b6f8a8975ad98ecf231
Shivam <shivamkr.vatsa@gmail.com>
2022-10-08 09:01:44 +0530

    detailed c programmes added

--- mm_c4.cpp
+++ mm_c4.cpp
@@ -24,8 +24,8 @@ int main(int argc, const char *argv[]) {
 
     struct timeval start, end;
     gettimeofday(&start, NULL);
-
-    cilk_for(int i = 0; i < n; ++i) {
+    int i;
+    cilk_for(i = 0; i < n; i++) {
         for (int k = 0; k < n; ++k) {
             for (int j = 0; j < n; ++j) {
                 C[i][j] += A[i][k] * B[k][j];
